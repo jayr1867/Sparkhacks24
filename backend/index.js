2f08d2a44abcf6f9265ed3fd585c4de6aecf2ba9
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2024-02-09 18:55:00 -0600

    init

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+app.use(cors());
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

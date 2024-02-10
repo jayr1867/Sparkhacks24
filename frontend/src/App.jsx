e32cbee74fd9623f6ce282d15b7ca6cd87789512
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2024-02-09 21:02:02 -0600

    linting and prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-        {/* <button onClick={() => fetch('http://localhost:3000/').then((res) => res.json()).then((data) => console.log(data.message))}> */}
+          {/* <button onClick={() => fetch('http://localhost:3000/').then((res) => res.json()).then((data) => console.log(data.message))}> */}
           count is {count}
         </button>
         <p>
@@ -30,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

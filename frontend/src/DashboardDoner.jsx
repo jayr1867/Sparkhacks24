ad165450a5ba5d9fd67940023dba39929c828a03
Jimmy <jaymeet5087d@gmail.com>
2024-02-10 00:17:45 -0600

    Doner dashboard basic functionality without css full implementation and without user info popup

---
+++ frontend/src/DashboardDoner.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./DashboardDoner.css";
+
+function DashhboardDoner() {
+  return (
+    <div className="container">
+      <div className="logoutbtn">
+        {/* adding + sign img */}
+        <button type="button" className="lgot_btn">
+          Logout
+        </button>
+      </div>
+      <div className="profilebtn">
+        {/* adding + sign img */}
+        <button type="button" className="prof_btn">
+          img
+        </button>
+      </div>
+      <div className="reqbtn">
+        {/* adding + sign img */}
+        <button type="button" className="new_donation_btn">
+          New Donation
+        </button>
+      </div>
+      <div className="active_requests_header">Active requests</div>
+      <div className="active_requests_table">
+        <table className="data_active">
+          <th className="table_header">
+            <td className="id">ID</td>
+            <td className="request_details"> Request Details</td>
+            <td className="status">Status</td>
+            <td className="expiry">Expiry</td>
+          </th>
+          <tr className="table_data">
+            <td>id</td>
+            <td>adbjhavdsjhsdvkhsfbvgksfbvlfdn</td>
+            <td>Active</td>
+            <td>Expiry</td>
+          </tr>
+        </table>
+      </div>
+      <div className="other_requests_header">Other requests requests</div>
+      <table className="data_others">{/* <tr><td>data to be fetched from table</td></tr> */}</table>
+    </div>
+  );
+}
+
+export default DashhboardDoner;

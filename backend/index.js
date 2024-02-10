3903ebc9eeccaa45fb9a960521490718800b5b91
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2024-02-09 21:05:50 -0600

    prettier

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 // const bodyParser = require('body-parser');
 app.use(cors());
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {

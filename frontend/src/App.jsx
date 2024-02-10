fc9df961007eac7702aa3ae9a91ff33c41dae487
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2024-02-10 14:05:14 -0600

    hashrouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 // import Ribbon from "./Ribbon.jsx";
 
 // import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Home.jsx";
 import DashboardDoner from "./DashboardDoner.jsx";

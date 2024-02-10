27f4c7690f5232ac5c7ca80139b20be7d6fb3d77
Jimmy <jaymeet5087d@gmail.com>
2024-02-10 10:39:09 -0600

    new donations popup created

---
+++ frontend/src/NewDonationsPopup.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NewDonationsPopup.css"
+
+function NewDonationsPopup(props) {
+  return props.trigger ? (
+    <div className= "NewDonationsPopup">
+      <div className="ndpopup-inner">
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          Close
+        </button>
+        {props.children}
+      </div>
+    </div>
+  ) : (
+    ""
+  );
+}
+
+export default NewDonationsPopup;

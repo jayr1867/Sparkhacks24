a62de550415c11a0770e5390651729d780496da2
VanditShah <vandituvrs@gmail.com>
2024-02-10 03:45:44 -0600

    Changed About

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -3,15 +3,17 @@ function About() {
     <div className="fading-effect">
       <h2>About Us</h2>
       <p>
-        Welcome to AAHAR, a community-driven platform that connects donors with
-        NGOs to combat hunger and reduce food waste. Our mission is to
-        facilitate the donation of surplus food, enabling individuals and
-        businesses to easily contribute to those in need. Through seamless
-        coordination with dedicated NGOs, we ensure that every meal serves its
-        purpose, nourishing the needy and fostering a culture of sustainability.
-        Join us in creating a world where generosity meets action, transforming
-        surplus into support and making a tangible difference in the fight
-        against hunger.
+        Welcome to AAHAR (आहार), meaning food in Hindi and Sanskrit, a
+        community-driven platform that connects donors with NGOs to combat
+        hunger and reduce food waste. Our mission is to facilitate the donation
+        of surplus food, enabling individuals and businesses to easily
+        contribute to those in need. Through seamless coordination with
+        dedicated NGOs, we ensure that every meal serves its purpose, nourishing
+        the needy and fostering a culture of sustainability. Join us in creating
+        a world where generosity meets action, transforming surplus into support
+        and making a tangible difference in the fight against hunger. Embrace
+        the spirit of AAHAR, where every contribution is a step towards a better
+        tomorrow.
       </p>
     </div>
   );

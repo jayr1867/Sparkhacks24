6282bc628798d96633df4b1f6567c6ebfa43209a
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2024-02-10 14:43:31 -0600

    config try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  rollupOptions: {
+    external: ["react", "react-router", "react-router-dom"],
+    output: {
+      globals: {
+        react: "React",
+      },
+    },
+  },
 });

cd57271875e30ee80ed789be19a599b3f2537b6f
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2024-02-10 01:41:48 -0600

    Added Google OAuth and session management

---
+++ backend/auth.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5050/auth/google/callback",
+    },
+    function (request, accessToken, refreshToken, profile, cb) {
+      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
+      return cb(null, profile);
+    },
+  ),
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,74 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const session = require("express-session");
+require("./auth");
+
+const crypto = require("crypto");
+
+// Generate a random string of a given length
+const generateRandomString = (length) => {
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString("hex")
+    .slice(0, length);
+};
+
+// Use the generated string as your session secret
+const sessionSecret = generateRandomString(32);
+
+app.use(
+  session({ secret: sessionSecret, resave: false, saveUninitialized: true }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+function ensureAuthenticated(req, res, next) {
+  if (req.user) {
+    return next();
+  }
+  res.send(401);
+}
+
+app.use(bodyParser.json());
 app.use(cors());
-const port = 3000;
+const port = 5050;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] }),
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/test",
+    failureRedirect: "auth/failure",
+  }),
+);
+
+app.get("/auth/failure", (req, res) => {
+  res.json({ message: "failure" });
+});
+
+app.get("/logout", (req, res) => {
+  req.logout(req.user, (err) => {
+    if (err) {
+      return res.send("Error logging out");
+    }
+  });
+  // req.session.destroy();
+  res.send("Goodbye!");
+});
+
+app.get("/test", ensureAuthenticated, (req, res) => {
+  res.json({ message: "Test" });
 });
 
 app.listen(port, () => {

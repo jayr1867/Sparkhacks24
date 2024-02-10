ddfd3a606c41dc8cf1cb98605b980ac829cfc80b
Jaymeet Patel <54699147+Jaymeet2003@users.noreply.github.com>
2024-02-10 04:07:30 -0600

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from "react";
+import "./App.css";
+import Background from "./Background.jsx";
+import About from "./About.jsx";
+import Ribbon from "./Ribbon.jsx";
+function App() {
+  return (
+    <>
+      <Ribbon />
+      <Background />
+      <About />
+    </>
+  );
+}
+
+export default App;

e77530447dba7725787325216d858040952044fe
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2024-02-10 13:37:26 -0600

    Add sign-in functionality to Ribbon component

--- frontend/src/Ribbon.jsx
+++ frontend/src/Ribbon.jsx
@@ -1,5 +1,9 @@
 import logo from "../public/logo.jpg"; // Update the path to your logo image
 
+const signIn = () => {
+  window.open("https://aahar-server.onrender.com/auth/google", "_self")
+}
+
 const Ribbon = () => {
   return (
     <div className="ribbon">
@@ -7,7 +11,7 @@ const Ribbon = () => {
         <img src={logo} alt="Logo" className="ribbon-logo" />
         <span>AAHAR</span>
       </div>
-      <button className="ribbon-button">Sign Up/Sign In</button>
+      <button className="ribbon-button" onClick={signIn}>Sign Up/Sign In</button>
     </div>
   );
 };
